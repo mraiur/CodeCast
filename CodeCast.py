5a9ced13279825c96ff37c61da1b070b3d864ab3
Nikolai Ivanov <mraiur@gmail.com>
2015-05-24 04:32:02 +0300

    Added preferences. Added menu buttons to connect/disconnect to the socket server.

--- CodeCast.py
+++ CodeCast.py
@@ -1,32 +1,104 @@
-import sublime, sublime_plugin, pickle, json, socket
-HOST = "localhost"
-PORT = 5005
+import sublime
+import sublime_plugin
+import pickle
+import json
+import socket
+
+settings = sublime.load_settings("CodeCast.sublime-settings")
+
+HOST = settings.get("SOCKET_SERVER_HOST", "localhost")
+PORT = settings.get("SOCKET_SERVER_PORT", 5005)
+AUTO_CONNECT = settings.get("AUTO_CONNECT", False)
+
+sock = False;
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-  
-class EventDump(sublime_plugin.EventListener):
+class CodeCastConnectCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.connect()
+
+    def connect(self):
+        global sock
+        
+        print('CodeCast Connecting... ', HOST, PORT)
+
+        try:
+            print('CodeCast Connected')
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((HOST, PORT))
+        except Exception as e:
+            sock = False;
+            print("CodeCast no server")
+
+class CodeCastDisconnectCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.disconnect()
+
+    def disconnect(self):
+        global sock
+        
+        print('CodeCast disconnecting... ', HOST, PORT)
+
+        if sock is not False:
+            sock.close();
+            sock = False;
+
+
+if AUTO_CONNECT is True:
+    o = CodeCastConnectCommand(sublime_plugin.TextCommand);
+    o.connect();
+
+class CodeCastCommand(sublime_plugin.EventListener):
+    def msg(self, data):
+        global sock
+
+        if sock is not False:
+            try:
+                sock.send(bytes(json.dumps(data), 'UTF-8'))
+            except Exception as e:
+                sock = False
+
     def on_selection_modified(self, view):
         
         sel = view.sel()
         region1 = sel[0]
         selectionText = view.substr(region1)
 
-        '''a = dict( file=view.file_name(), selection = selectionText);'''
-        a = {"file": view.file_name(), "selection" : selectionText}
+        data = {
+            "action": "select",
+            "file": view.file_name(),
+            "data" : selectionText,
+            "selection": { 
+                "start": sel[0].a,
+                "end": sel[0].b
+            }
+        }
+        self.msg(data)
+        view.set_status('_filename', self.connectionInformation() )
 
-        s.send(bytes(json.dumps(a), 'UTF-8'))
-        print(a)
-  
-    def onSelectionModified(self, view):  
-        print("modified a")
+    def on_modified(self, view):
+        region = sublime.Region(0, view.size());
 
-    def onSelectionModifiedAsync(self, view):
-        print("async modified")
-  
-    def onActivated(self, view):  
-        print("is now the active view")
+        data = {
+            "action": "update",
+            "file": view.file_name(),
+            "data": view.substr(region)
+        }
+        self.msg(data)
+
+    def connectionInformation(self):
+        global sock, HOST, PORT
+        if sock is False:
+            return ""
+        else:
+            return "Connected to "+HOST+":"+str(PORT)
+    
+    def on_activated(self, view):  
+        if view.file_name():
+            fileHandler = open(view.file_name(), encoding='utf-8');
+            contents = fileHandler.read();
+            data = {"action": "changefile", "file": view.file_name(), "data": contents }
+            self.msg(data)
   
     def onClose(self, view):  
         print("is no more"  )
